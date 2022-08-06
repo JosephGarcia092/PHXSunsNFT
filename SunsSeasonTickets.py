f6c725d9c0f3f74f21cf39bc009d374f671906fa
Rachel Carroll <98990090+RachelCarroll2018@users.noreply.github.com>
2022-08-06 12:53:11 -0700

    updated .py

--- SunsSeasonTickets.py
+++ SunsSeasonTickets.py
@@ -1,77 +1,103 @@
-import streamlit as st 
-from mnemonic import Mnemonic 
-from bip44 import Wallet
-from web3 import Account
-import os
-from dotenv import load_dotenv
-load_dotenv()
+    import streamlit as st 
+    from mnemonic import Mnemonic 
+    from bip44 import Wallet
+    from web3 import Account
+    import os
+    from dotenv import load_dotenv
+    load_dotenv()
 
-####################
-# import functions
-from Mnemonic import generate_mnemonic
-contract = load_contract()
-import os
-import json
-from web3 import Web3
-from pathlib import Path
-from dotenv import load_dotenv
-import streamlit as st
-load_dotenv()
+    ####################
+    # import functions
+    from Mnemonic import generate_mnemonic
 
-# Define and connect a new Web3 provider
-w3 = Web3(Web3.HTTPProvider(os.getenv("WEB3_PROVIDER_URI")))
+    # Define and connect a new Web3 provider
+    w3 = Web3(Web3.HTTPProvider(os.getenv("WEB3_PROVIDER_URI")))
 
-@st.cache(allow_output_mutation=True)
-def load_contract():
+    @st.cache(allow_output_mutation=True)
+    def load_contract():
 
-    #### THIS NEEDS TO BE UPDATED
-    # Load the contract ABI
-    with open(Path('Desktop/PHXSunsNFT/compiled/trial.json')) as f:
-        artwork_abi = json.load(f)
+        #### THIS NEEDS TO BE UPDATED
+        # Load the contract ABI
+        with open(Path('./contracts/compiled/artwork_abi.json')) as f:
+            artwork_abi = json.load(f)
 
-    contract_address = os.getenv("SMART_CONTRACT_ADDRESS")
+        contract_address = os.getenv("SMART_CONTRACT_ADDRESS")
 
-    # Load the contract
-    contract = w3.eth.contract(
-        address=contract_address,
-        abi=artwork_abi
-    )
+        # Load the contract
+        contract = w3.eth.contract(
+            address=contract_address,
+            abi=artwork_abi
+        )
 
-    return contract
+        return contract
+
+    contract = load_contract()
+
+    # Define and connect a new Web3 provider
+    w3 = Web3(Web3.HTTPProvider(os.getenv("WEB3_PROVIDER_URI")))
+
+    @st.cache(allow_output_mutation=True)
+    def load_contract():
+
+        #### THIS NEEDS TO BE UPDATED
+        # Load the contract ABI
+        with open(Path('./contracts/compiled/artwork_abi.json')) as f:
+            artwork_abi = json.load(f)
+
+        contract_address = os.getenv("SMART_CONTRACT_ADDRESS")
+
+        # Load the contract
+        contract = w3.eth.contract(
+            address=contract_address,
+            abi=artwork_abi
+        )
+
+        return contract
+st.set_page_config(layout="wide")
 
 st.title('Phoenix Suns 2023 Season Tickets')
 
 st.header('Welcome to the Future! Introducing NFT Season Tickets!')
-
-col4, col5 = st.columns(2)
-col4.metric("Wins", "64", "1.2 °F")
-col5.metric("Losses", "18", "-8%")
+st.subheader("Upon purchase of your package you will be able to generate your own NFT! Each NFT will be unique and individual to you, make sure you hold on to them because you will receive a discount on all future merchandise.")
 
 col1, col2, col3 = st.columns(3)
 
 with col1:
     st.header("Standard Package")
-    st.write("Tickets and NFT, and 5% off Merchandise for the Season")
-    st.image("https://cdn.dribbble.com/users/412468/screenshots/16900443/fdcce1cf-9f49-45d2-a67d-4909891cbe6c.png")
+    st.write("")
+    st.write("Game ticket, NFT, and 5% off Merchandise for the Season")
+    st.write("Tickets will be available in Section 106 - 110 or 117 - 123")
+    st.write("")
+    st.write("")    
+    st.image("https://cdn.dribbble.com/users/412468/screenshots/16900443/fdcce1cf-9f49-45d2-a67d-4909891cbe6c.png?compress=1&resize=500x400")
 
 with col2:
     st.header("Ultra Package")
-    st.write("Game tickets, $40 F&B Package, NFT, and 10% off Merchandise for the Season")
-    st.image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRmQVCFrB94NgfBTfUmDLRG8zxxvpKol4In3e744IS31p3M_DRHH4LPMqTSvqNOrdO1Qc&usqp=CAU")
+    st.write("")
+    st.write("Game ticket, $40 F&B Package, NFT, and 10% off Merchandise for the Season")
+    st.write("Tickets will be available in Section 101 - 105 or 111 - 116")
+    st.write("")
+    st.write("")    
+    st.image("https://cdn.dribbble.com/users/1785078/screenshots/3644361/media/bbe8083c5426965690c4c0db01803947.png?compress=1&resize=500x400")
 
 with col3:
     st.header("VIP Package")
-    st.write("Game tickets, $$100 F&B Package, Photo with Mascot, Raffle for $10k, NFT, and 15% off Merchandise for the Season")
-    st.image("https://assets.simpleviewinc.com/simpleview/image/fetch/c_fill,h_690,q_70,w_1040/https://assets.simpleviewinc.com/simpleview/image/upload/crm/chandler/Phoenix-Suns-Logo_A84287DC-5056-A36A-0B2C90380AD9B0DC-a842865c5056a36_a8428fc1-5056-a36a-0be758710200bec8.jpg")
+    st.write("Game ticket, $$100 F&B Package, Photo with Mascot, Raffle for $10k, NFT, and 15% off Merchandise for the Season")
+    st.write("Tickets will be available in Section 101 - 105 or 111 - 116 lower levels (not including courtside or rows 1-8)")
+    st.image("https://cdn.dribbble.com/users/51266/screenshots/3916926/phoenix_basketball.png?compress=1&resize=500x400")
+    
+col4, col5 = st.columns(2)
+col4.metric("Wins ('21-'22)", "64", "13")
+col5.metric("Losses ('21-'22)", "18", "-3", delta_color="inverse")
 
-standard_image = "https://cdn.dribbble.com/users/412468/screenshots/16900443/fdcce1cf-9f49-45d2-a67d-4909891cbe6c.png"
-ultra_image = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQRmQVCFrB94NgfBTfUmDLRG8zxxvpKol4In3e744IS31p3M_DRHH4LPMqTSvqNOrdO1Qc&usqp=CAU"
-vip_image = "https://assets.simpleviewinc.com/simpleview/image/fetch/c_fill,h_690,q_70,w_1040/https://assets.simpleviewinc.com/simpleview/image/upload/crm/chandler/Phoenix-Suns-Logo_A84287DC-5056-A36A-0B2C90380AD9B0DC-a842865c5056a36_a8428fc1-5056-a36a-0be758710200bec8.jpg"
+standard_image = "https://cdn.dribbble.com/users/412468/screenshots/16900443/fdcce1cf-9f49-45d2-a67d-4909891cbe6c.png?compress=1&resize=500x400"
+ultra_image = "https://cdn.dribbble.com/users/1785078/screenshots/3644361/media/bbe8083c5426965690c4c0db01803947.png?compress=1&resize=400x300"
+vip_image = "https://cdn.dribbble.com/users/51266/screenshots/3916926/phoenix_basketball.png?compress=1&resize=500x400"
 
 packages_database = {
-    "Standard Package": ["Standard Package", .05, standard_image, "Tickets and NFT, and 5% off Merchandise for the Season"],
-    "Ultra Package": ["Ultra Package", .08, ultra_image, "Game tickets, $40 F&B Package, NFT, and 10% off Merchandise for the Season"],
-    "VIP Package": ["VIP Package", .09, vip_image, "Game tickets, $$100 F&B Package, Photo with Mascot, Raffle for $10k, NFT, and 15% off Merchandise for the Season"]}
+    "Standard Package": ["Standard Package", .05, standard_image, "Game ticket, NFT, and 5% off Merchandise for the Season"],
+    "Ultra Package": ["Ultra Package", .08, ultra_image, "Game ticket, $40 F&B Package, NFT, and 10% off Merchandise for the Season"],
+    "VIP Package": ["VIP Package", .09, vip_image, "Game ticket, $$100 F&B Package, Photo with Mascot, Raffle for $10k, NFT, and 15% off Merchandise for the Season"]}
 
 packages = ["Standard Package", "Ultra Package", "VIP Package"]
 
@@ -98,3 +124,9 @@ st.sidebar.write(included)
 st.sidebar.markdown("## Cost in Ether")
 cost = packages_database[select_package][1]
 st.sidebar.write(cost)
+    
+### Generate NFT button
+transaction_id = st.number_input("Enter transaction hash id to display:", step=1)
+if st.button("Generate NFT"):
+    nft_owner = contract.functions.ownerOf(transaction_id).call()
+    st.write(f"This NFT is awarded to {transaction_id}")
